4668234a9d977ef329b1959248c76e2c6a20ad4f
Alexis Jacob <ferjacobka@gmail.com>
2026-08-26 19:59:07 -0400

    chore: configs routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ReservaComponent } from './components/reserva/reserva.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ReservaComponent }, // Tu pantalla principal será Reserva
+  { path: '**', redirectTo: '' },
+];
